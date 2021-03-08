2b2dbeff95eb8bcc830fd080f0cc7c2a5ce08437
Taso <taso@projecterror.dev>
2021-03-07 22:43:12 -0500

    fix(docs): Fixed edit page resolution

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,8 @@ module.exports = {
           showLastUpdateTime: true,
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/project-error/docs.projecterror.dev",
+          editUrl:
+            "https://github.com/project-error/docs.projecterror.dev/tree/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
